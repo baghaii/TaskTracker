808224ae045c7b42fe227e9d40b6935c121b034f
Baghaii <baghaii@gmail.com>
2015-10-14 14:52:47 -0600

    Created a notification

---
+++ app/src/main/java/com/androidatelier/tasktracker/NotificationReceiverActivity.java
@@ -0,0 +1,18 @@
+package com.androidatelier.tasktracker;
+
+/**
+ * Created by baghaii on 9/21/15.
+ */
+
+import android.app.Activity;
+
+import android.os.Bundle;
+
+
+public class NotificationReceiverActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.result);
+    }
+}
